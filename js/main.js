78e3d51f3e4ca9497882577463ac75427f5cc074
Kyungsuh Park <pyo05360@gmail.com>
2022-03-09 14:48:27 +0900

    댓글 기능 구현

--- js/main.js
+++ js/main.js
@@ -5,11 +5,6 @@ console.log(btnSave)
 const commentBox = document.querySelector(".commentBox")
 console.log(commentBox)
 const form = document.querySelector('form');
-// let itemsArray = localStorage.getItem('items')?JSON.parse(localStorage.getItem('items')):[];
-
-// localStorage.setItem('items',JSON.stringify(itemsArray));
-// const data = JSON.parse(localStorage.getItem('items'));
-// console.log(data);
 
 const listMaker = (text) => {
     const div = document.createElement('div');
@@ -19,8 +14,12 @@ const listMaker = (text) => {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault(); 
-    itemsArray.push(input.value);
-    // localStorage.setItem('items',JSON.stringify(itemsArray));
-    listMaker(input.value); // input 값을 받아온다 
-    input.value="";  // input은 다시 공백으로 만들기 
+    listMaker(input.value); 
+    input.value="";  
 });
+
+commentBox.addEventListener('click', () => {
+    e.preventDefault(); 
+    listMaker(input.value); 
+    input.value=""; 
+})
